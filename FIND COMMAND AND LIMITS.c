f92f08203fd78e52df48be0964f21dda3830aad1
sai rama narayana reddy . sabbella <92290652+saisabbella@users.noreply.github.com>
2021-10-27 12:06:21 +0530

    Add files via upload

---
+++ FIND COMMAND AND LIMITS.c
@@ -0,0 +1,14 @@
+//*FIND COMMAND AND LIMITS//*
+#include<stdio.h>
+#include<limits.h>
+void main()
+{
+	printf("short int:\nsigned:%hd to %hd\n",SHRT_MIN,SHRT_MAX);
+	printf("unsigned: %d to %hu",0, USHRT_MAX);
+	printf("\nINT:\nsigned:%d to %d\n",INT_MIN,INT_MAX);
+	printf("unsigned:%u to %u\n",0,UINT_MAX);
+	printf("Long int:\nsigned:%ld to %ld\n",LONG_MIN, LONG_MAX);
+	printf("unsigned:%lu to %lu\n",0,ULONG_MAX);
+	printf("Long Long int:\nsigned:%lld to %lld\n",LONG_LONG_MIN, LONG_LONG_MAX);
+	printf("unsigned:%llu to %llu\n",0,ULONG_LONG_MAX);
+}
