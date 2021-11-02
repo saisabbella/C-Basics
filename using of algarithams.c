2da70b0aa4d6cf885d100c71ec37978c2b89abbf
sai rama narayana reddy . sabbella <92290652+saisabbella@users.noreply.github.com>
2021-11-02 12:22:42 +0530

    Add files via upload

---
+++ using of algarithams.c
@@ -0,0 +1,19 @@
+/* using of algarithams */
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d,e,f;
+	
+	a=12;
+	b=12;
+	
+	c=a+b;
+	d=a-b;
+	e=a*b;
+	f=a*1.0/b;
+	printf("sum of %d and %d is %d",a,b,c);
+	printf("\nsubtration of %d and %d is %d",a,b,d);
+	printf("\nmultiplication of %d and %d is %d",a,b,e);
+	printf("\ndivision of %d and %d is %d",a,b,f);
+	
+}
