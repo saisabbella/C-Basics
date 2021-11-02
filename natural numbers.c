ede5d1ce52439fb445f348d5f1c4e017c06efac1
sai rama narayana reddy . sabbella <92290652+saisabbella@users.noreply.github.com>
2021-11-02 12:20:29 +0530

    Add files via upload

---
+++ natural numbers.c
@@ -0,0 +1,13 @@
+/* natural numbers */
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d;
+	
+	a=5;
+	c=10;
+	b=a*(a+1)/2;
+	d=c*(c+1)/2;
+	printf("sum of first %d natural number is: %d",a,b);
+	printf("\nsum of first %d natural number is: %d",c,d);
+}
