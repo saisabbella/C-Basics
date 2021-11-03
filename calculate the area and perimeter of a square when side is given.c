3fd1bf35b621ec66d70a89d191d4f370c42849ed
sai rama narayana reddy . sabbella <92290652+saisabbella@users.noreply.github.com>
2021-11-03 12:07:38 +0530

    Add files via upload

---
+++ calculate the area and perimeter of a square when side is given.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int side,area,perimeter;
+	side=4;
+	area=side*side;
+	perimeter=4*side;
+    printf("area of the square with side %d is %d",side,area);
+    printf("\narea of the perimeter with side %d is %d",side,perimeter);
+   
+}
+    
+
+	
+
