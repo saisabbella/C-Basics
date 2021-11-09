501d4e0ef498274a1eebce478c3f4a89a27427c8
sai rama narayana reddy . sabbella <92290652+saisabbella@users.noreply.github.com>
2021-11-09 12:28:57 +0530

    Add files via upload

---
+++ enter three numbers using printf and scanf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	/*printf("enter three numbers");
+	scanf("%d%d%d",&a,&b,&c);*/
+	         /*(or)*/
+	printf("enter 1st number");
+	scanf("%d",&a);
+	printf("enter 2st number");
+	scanf("%d",&b);
+	printf("enter 3st number");
+	scanf("%d",&c);
+	printf("number1: %d\n",a);
+	printf("number2: %d\n",b);
+	printf("number3: %d\n",c);
+}
---
+++ square of numbers.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("ENTER A NUMBER:");
+	scanf("%d",&n);
+	printf("square is: %d",n*n);
+}
